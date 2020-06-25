9397b8eb70a2d1bd3be49d38ceb238c7c4cdfec7
Brian <brianmkcebm@gmail.com>
2020-06-25 17:06:32 +0100

    commit new notebook

--- notebooks/diffable_python/chlorquine.not.hcq.py
+++ notebooks/diffable_python/chlorquine.not.hcq.py
@@ -14,13 +14,18 @@
 #     name: python3
 # ---
 
+# This notebook contains dm+d codes medications chloroquine phosphate or chloroquine sulfate. Prescribing of these products in general practice is minimal and you can see current prescribing patterns on OpenPrescribing
+# - [Chloroquine phosphate](https://openprescribing.net/chemical/0504010F0/)
+# - [Chloroquine with proguanil](https://openprescribing.net/chemical/0504010Z0/)
+# - [Chloroquine sulphate](https://openprescribing.net/chemical/0504010G0/)
+
 from ebmdatalab import bq
+import pandas as pd
 import os
 
 # +
 sql = '''WITH bnf_codes AS (
   SELECT bnf_code FROM hscic.presentation WHERE 
-    bnf_code LIKE '1001030C0%' OR ##hydroxychloroquine sulfate - BNF sect drugs used in rheumatic disease
     bnf_code LIKE '0504010F0%' OR ## chloroquine phosphate - BNF sect antiprotozoal drugs
     bnf_code LIKE '0504010Z0%'  OR ## chloroquine phosphate with proguanil - BNF sect antiprotozoal drugs
     bnf_code LIKE '0504010G0%' ## chloroquine sulfate - BNF sect antiprotozoal drugs 
@@ -39,7 +44,6 @@ WHERE bnf_code IN (SELECT * FROM bnf_codes)
 ORDER BY type, bnf_code, id'''
 
 chloroquine_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','chloroquine_codelist.csv'))
-chloroquine_codelist.head(10)
-# -
-
-chloroquine_codelist.info()
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+chloroquine_codelist
